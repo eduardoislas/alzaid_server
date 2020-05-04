12da07548b0cbc910b869d8178bfdfa026cdf156
eduardoislas <eislas22@gmail.com>
2020-05-04 03:30:23 -0700

    pruebas activitydone

--- routes/binnacle.js
+++ routes/binnacle.js
@@ -274,13 +274,11 @@ app.get('/binnacle/patient/activitydone/:idp&:ida', (req, res) => {
                     success: false,
                     pa
                 })
-            } else {
-                res.json({
-                    success: true,
-                    pa
-                });
             }
-
+            res.json({
+                success: true,
+                pa
+            });
         });
 })
 
