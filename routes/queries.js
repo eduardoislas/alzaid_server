17b1f01431f136f71c82691ee49e283510d5e27e
eduardoislas <eislas22@gmail.com>
2021-03-29 22:17:33 -0700

    corrección en queries

--- routes/queries.js
+++ routes/queries.js
@@ -2,7 +2,7 @@ const express = require('express');
 const json2csv = require('json2csv').parse;
 
 const DailyRecord = require('../models/dailyRecord');
-const Record = require('../models/Record');
+const Record = require('../models/record');
 
 const app = express();
 
